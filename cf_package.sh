a092f959a961c31fd21dce7241b354f702f4aca9
Frederic Lehmann <frederic.lehmann@transdev.com>
2018-03-21 18:05:15 +0000

    Nouvelle arbo serverless

--- cf_package.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation package \
-   --template-file /home/ec2-user/environment/FinOps/template.yaml \
-   --output-template-file git_ignore/serverless-output.yaml \
-   --s3-bucket pprod-deploy
